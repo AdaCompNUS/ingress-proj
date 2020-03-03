378a457750bf8c25bdaaac41a4ad4d88c3cdae87
lyf44 <luyunfan44@gmail.com>
2020-03-03 14:18:15 +0800

    <Ingress> Added test_ingress_file test script
    Changes:
    1. Added test_ingress_file test script which takes in an image file and asks ingress to generate captions
    
    Test & Result:
    Tested. No Error

--- ingress_ros/src/ingress_srv/ingress_srv.py
+++ ingress_ros/src/ingress_srv/ingress_srv.py
@@ -19,36 +19,42 @@ from datetime import datetime
 import action_controller.msg
 
 '''Settings'''
-VALID_MIN_CLUSTER_SIZE = 500 # points
+VALID_MIN_CLUSTER_SIZE = 500  # points
 VALID_MIN_RELEVANCY_SCORE = 0.05
 
 DEBUG = False
+
 
 def dbg_print(text):
     if DEBUG:
         print(text)
 
+
 class Ingress():
     def __init__(self):
         # wait for grounding load server
-        self._load_client = actionlib.SimpleActionClient('dense_refexp_load', action_controller.msg.DenseRefexpLoadAction)
+        self._load_client = actionlib.SimpleActionClient(
+            'dense_refexp_load', action_controller.msg.DenseRefexpLoadAction)
         rospy.loginfo("DemoManager: 1. Waiting for dense_refexp_load action server ...")
         self._self_captions = []
         self._load_client.wait_for_server()
 
         # wait for relevancy clustering server
-        self._relevancy_client = actionlib.SimpleActionClient('relevancy_clustering', action_controller.msg.RelevancyClusteringAction)
+        self._relevancy_client = actionlib.SimpleActionClient(
+            'relevancy_clustering', action_controller.msg.RelevancyClusteringAction)
         rospy.loginfo("DemoManager: 2. Waiting for relevancy_clustering action server ...")
-        self._relevancy_client.wait_for_server()    
+        self._relevancy_client.wait_for_server()
 
         # wait for grounding query server
-        self._query_client = actionlib.SimpleActionClient('boxes_refexp_query', action_controller.msg.BoxesRefexpQueryAction)
+        self._query_client = actionlib.SimpleActionClient(
+            'boxes_refexp_query', action_controller.msg.BoxesRefexpQueryAction)
         rospy.loginfo("DemoManager: 3. Waiting for boxes_refexp_query action server ...")
         self._rel_captions = []
         self._query_client.wait_for_server()
 
         # publisher for Ingress grounding results
-        self._grounding_result_pub = rospy.Publisher('ingress/dense_refexp_result', Image, queue_size=10, latch=True)
+        self._grounding_result_pub = rospy.Publisher(
+            'ingress/dense_refexp_result', Image, queue_size=10, latch=True)
 
         # demo ready!
         rospy.loginfo("Ingress ready!")
@@ -68,8 +74,8 @@ class Ingress():
         rospy.loginfo("_ground_load: getting result")
         load_result = self._load_client.get_result()
 
-        boxes = np.reshape(load_result.boxes, (-1, 4)) 
-        losses = np.array(load_result.scores)     
+        boxes = np.reshape(load_result.boxes, (-1, 4))
+        losses = np.array(load_result.scores)
         self._ungrounded_captions = np.array(load_result.captions)
 
         return boxes, losses
@@ -82,10 +88,11 @@ class Ingress():
         all_orig_idx = relevancy_result.all_orig_idx
 
         semantic_softmax = np.array(relevancy_result.softmax_probs)
-        semantic_softmax_orig_idxs = [-1.] * len(all_orig_idx) # this is disgusting.... puke! You should be ashamed of yourself Mohit.
+        # this is disgusting.... puke! You should be ashamed of yourself Mohit.
+        semantic_softmax_orig_idxs = [-1.] * len(all_orig_idx)
         for count, idx in enumerate(all_orig_idx):
             semantic_softmax_orig_idxs[idx] = semantic_softmax[count]
-            
+
         top_idxs = context_boxes_idxs
         spatial_captions = query_result.predicted_captions
         spatial_softmax = np.array(query_result.probs) / np.sum(np.array(query_result.probs))
@@ -105,13 +112,13 @@ class Ingress():
                 sem_captions.append(self._ungrounded_captions[idx])
                 sem_probs.append(semantic_softmax_orig_idxs[idx])
                 if count < len(spatial_softmax) and count < len(spatial_captions):
-                    rel_captions.append(spatial_captions[count].replace(query, '').replace('.', '').strip())
+                    rel_captions.append(spatial_captions[count].replace(
+                        query, '').replace('.', '').strip())
                     rel_probs.append(spatial_softmax[count])
                 else:
                     rospy.logwarn("Semantic and Spatial captions+prob lengths dont match!")
-                
-        return [sem_captions, sem_probs, rel_captions, rel_probs]
 
+        return [sem_captions, sem_probs, rel_captions, rel_probs]
 
     def _ground_query(self, expr, boxes):
         '''
@@ -123,13 +130,13 @@ class Ingress():
         query = expr
         rospy.loginfo("Grounding Input String: " + query)
 
-        # send expression for relevancy clustering 
+        # send expression for relevancy clustering
         incorrect_idxs = []
-        
+
         # dialog storage
         self._relevancy_result = None
-        self._context_boxes_idxs =  None
-        
+        self._context_boxes_idxs = None
+
         # relevancy
         relevancy_goal = action_controller.msg.RelevancyClusteringGoal(query, incorrect_idxs)
         self._relevancy_client.send_goal(relevancy_goal)
@@ -146,15 +153,17 @@ class Ingress():
         # selection_orig_idx = list(np.take(selection_orig_idx, pruned_selection_orig_idx))
         # self._relevancy_result.selection_orig_idx = list(selection_orig_idx)
 
-        ## clean by threshold
+        # clean by threshold
         semantic_softmax = np.array(self._relevancy_result.softmax_probs)
 
         all_orig_idx = self._relevancy_result.all_orig_idx
-        semantic_softmax_orig_idxs = [-1.] * len(all_orig_idx) # this is disgusting.... puke! You should be ashamed of yourself Mohit.
+        # this is disgusting.... puke! You should be ashamed of yourself Mohit.
+        semantic_softmax_orig_idxs = [-1.] * len(all_orig_idx)
         for count, idx in enumerate(all_orig_idx):
             semantic_softmax_orig_idxs[idx] = semantic_softmax[count]
 
-        pruned_selection_orig_idx = [i for i, idx in enumerate(selection_orig_idx) if semantic_softmax_orig_idxs[idx] > VALID_MIN_RELEVANCY_SCORE]
+        pruned_selection_orig_idx = [i for i, idx in enumerate(
+            selection_orig_idx) if semantic_softmax_orig_idxs[idx] > VALID_MIN_RELEVANCY_SCORE]
         selection_orig_idx = list(np.take(selection_orig_idx, pruned_selection_orig_idx))
         self._relevancy_result.selection_orig_idx = selection_orig_idx
         rospy.loginfo("pruned index {}".format(selection_orig_idx))
@@ -165,7 +174,8 @@ class Ingress():
 
         # ground the query
         selected_boxes = np.take(boxes, selection_orig_idx, axis=0)
-        query_goal = action_controller.msg.BoxesRefexpQueryGoal(query, np.array(selected_boxes).ravel(), selection_orig_idx, incorrect_idxs)
+        query_goal = action_controller.msg.BoxesRefexpQueryGoal(
+            query, np.array(selected_boxes).ravel(), selection_orig_idx, incorrect_idxs)
         self._query_client.send_goal(query_goal)
         self._query_client.wait_for_result()
         query_result = self._query_client.get_result()
@@ -176,7 +186,8 @@ class Ingress():
         self._context_boxes_idxs = list(context_boxes_idxs)
 
         # preprocess data for POMDP model
-        pomdp_init_data = self._pomdp_preprocess(self._relevancy_result, query_result, context_boxes_idxs, query)
+        pomdp_init_data = self._pomdp_preprocess(
+            self._relevancy_result, query_result, context_boxes_idxs, query)
 
         return top_idx, context_boxes_idxs, pomdp_init_data
 
@@ -188,7 +199,8 @@ class Ingress():
 
         query = expr
         if self._relevancy_result == None:
-            rospy.logerr("Grounding server wasn't initialized. Can't reground without initial grounding from _ground_query function")
+            rospy.logerr(
+                "Grounding server wasn't initialized. Can't reground without initial grounding from _ground_query function")
             return None
 
         rospy.loginfo("Regrounding Input String: " + query)
@@ -196,18 +208,19 @@ class Ingress():
         if query == '':
             return None, None, [None, [1.0]*len(self._relevancy_result.selection_orig_idx), None, [1.0]*len(self._relevancy_result.selection_orig_idx)]
 
-        # send expression for relevancy clustering 
-        incorrect_idxs=[]
+        # send expression for relevancy clustering
+        incorrect_idxs = []
         relevancy_goal = action_controller.msg.RelevancyClusteringGoal(query, incorrect_idxs)
         self._relevancy_client.send_goal(relevancy_goal)
         self._relevancy_client.wait_for_result()
         new_relevancy_result = self._relevancy_client.get_result()
-        # selection_orig_idx = self._relevancy_client.get_result().selection_orig_idx   
+        # selection_orig_idx = self._relevancy_client.get_result().selection_orig_idx
 
         # ground the new query with old relevancy clustering
         selection_orig_idx = self._relevancy_result.selection_orig_idx
         selected_boxes = np.take(boxes, selection_orig_idx, axis=0)
-        query_goal = action_controller.msg.BoxesRefexpQueryGoal(query, np.array(selected_boxes).ravel(), selection_orig_idx, incorrect_idxs)
+        query_goal = action_controller.msg.BoxesRefexpQueryGoal(
+            query, np.array(selected_boxes).ravel(), selection_orig_idx, incorrect_idxs)
         self._query_client.send_goal(query_goal)
         self._query_client.wait_for_result()
         query_result = self._query_client.get_result()
@@ -217,21 +230,22 @@ class Ingress():
         context_boxes_idxs = [top_idx] + list(query_result.context_boxes_idxs)
 
         # preprocess data for POMDP model
-        pomdp_init_data = self._pomdp_preprocess(new_relevancy_result, query_result, self._context_boxes_idxs, query)
+        pomdp_init_data = self._pomdp_preprocess(
+            new_relevancy_result, query_result, self._context_boxes_idxs, query)
 
         return top_idx, context_boxes_idxs, pomdp_init_data
 
-    def _publish_grounding_result(self, boxes, context_boxes_idxs):
+    def _publish_grounding_result(self, boxes, context_boxes_idxs, pomdp_init_data=None):
         '''
         debugging visualization of the bounding box outputs from the grounding model
         '''
 
-        ## Input validity check
+        # Input validity check
         if len(boxes) == 0 or len(context_boxes_idxs) == 0:
             return
 
         # Deepcopy is important here, otherwise self._img_msg will be contaminated.
-        draw_img = copy.deepcopy(CvBridge().imgmsg_to_cv2(self._img_msg)) 
+        draw_img = copy.deepcopy(CvBridge().imgmsg_to_cv2(self._img_msg))
         for (count, idx) in enumerate(context_boxes_idxs):
 
             x1 = int(boxes[idx][0])
@@ -239,15 +253,27 @@ class Ingress():
             x2 = int(boxes[idx][0] + boxes[idx][2])
             y2 = int(boxes[idx][1] + boxes[idx][3])
 
-            if count == 0: # top idx
-                cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0,255,0), 15)
+            if count == 0:  # top idx
+                cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0, 255, 0), 5)
             elif count < len(context_boxes_idxs):
-                cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0,0,255), 11)
+                cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+            # add captions
+            if pomdp_init_data is not None:
+                sem_captions, _, _, _ = pomdp_init_data
+                font = cv2.FONT_HERSHEY_DUPLEX
+                print("x1 {}, y1 {}".format(x1, y1))
+                if y1 - 15 > 5:
+                    cv2.putText(draw_img, sem_captions[count],
+                                (x1 + 6, y1 - 15), font, 1, (255, 255, 255), 2)
+                else:
+                    cv2.putText(draw_img, sem_captions[count],
+                                (x1 + 6, y1 + 5), font, 1, (255, 255, 255), 2)
 
         # cv2.imshow('image', draw_img)
         # cv2.waitKey(50)
         # cv2.destroyAllWindows()
-        result_img = CvBridge().cv2_to_imgmsg(draw_img)
+        result_img = CvBridge().cv2_to_imgmsg(draw_img, encoding='bgr8')
         self._grounding_result_pub.publish(result_img)
         rospy.loginfo("grounding result published")
 
@@ -258,8 +284,8 @@ class Ingress():
         @param expr, string, user input to describe the target object
         @return. the most likely bounding boxes. 
         '''
-        
-        ## Input validity check
+
+        # Input validity check
         if image is None:
             rospy.logerr("DemoMovo/ground: image is none, skip")
             return None
@@ -270,20 +296,24 @@ class Ingress():
             rospy.loginfo("Ingress: empty query string received, returning ungrounded result")
             top_idx = 0
             context_idxs = [i for i in range(0, len(boxes)) if losses[i] > 0]
-            pomdp_init_data = ([self._ungrounded_captions[i] for i in context_idxs], [losses[i] for i in context_idxs], [], None)
+            pomdp_init_data = ([self._ungrounded_captions[i]
+                                for i in context_idxs], [losses[i] for i in context_idxs], [], None)
+            self._publish_grounding_result(
+                boxes, context_idxs, pomdp_init_data)  # visualization of RViz
             return boxes, top_idx, context_idxs, pomdp_init_data
 
         top_idx, context_idxs, pomdp_init_data = self._ground_query(expr, boxes)
         # context_idxs.append(top_idx)
 
-        self._publish_grounding_result(boxes, context_idxs) # visualization of RViz
+        self._publish_grounding_result(boxes, context_idxs)  # visualization of RViz
         return boxes, top_idx, context_idxs, pomdp_init_data
+
 
 if __name__ == '__main__':
     rospy.init_node('ingress_ros')
 
     try:
-        ingress = Ingress()   
+        ingress = Ingress()
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.logerr("ingress exit!!")
